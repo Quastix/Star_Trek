81f21567c72bf246714a4426b86750ceb540cf44
Quastix <98455497+Quastix@users.noreply.github.com>
2022-07-12 10:51:06 +0200

    Delete Frituur Frida oplossingen cursus directory

--- Frituur Frida oplossingen cursus/frida oplossingen cursus/src/main/java/be.vdab.frida/exceptions/SnackNietGevondenException.java
+++
@@ -1,5 +0,0 @@
-package be.vdab.frida.exceptions;
-
-public class SnackNietGevondenException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-}
--- Frituur Frida oplossingen cursus/frida oplossingen cursus/src/main/java/be.vdab.frida/repositories/SausRepository.java
+++
@@ -1,9 +0,0 @@
-package be.vdab.frida.repositories;
-
-import be.vdab.frida.domain.Saus;
-
-import java.util.stream.Stream;
-
-public interface SausRepository {
-    Stream<Saus> findAll();
-}
