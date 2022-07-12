0dc9cddaef0838a287f1a9e846b233fb73cf252f
Quastix <98455497+Quastix@users.noreply.github.com>
2022-07-12 10:52:35 +0200

    Delete TestenVanCode directory

--- TestenVanCode/src/be/vdab/Main.java
+++
@@ -1,9 +0,0 @@
-package be.vdab;
-
-import java.time.LocalDate;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(LocalDate.now().getDayOfWeek());
-    }
-}
